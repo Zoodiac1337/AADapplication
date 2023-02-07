fd6cf4265bd461c653cc19112f87db606fb11970
pawel <N0939175@my.ntu.ac.uk>
2023-02-07 16:13:59 +0000

    Implemented editing and removing existing users

--- app/src/main/java/com/example/aadapplication/editUserActivity.java
+++ app/src/main/java/com/example/aadapplication/editUserActivity.java
@@ -6,6 +6,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,25 +21,39 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
-public class addNewUserActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class editUserActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private String type;
     private String fridgeID;
     private String sEmail;
     private String sName;
+    private String[] items;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_user);
+        setContentView(R.layout.activity_edit_user);
+
+        TextView email = findViewById(R.id.textEmailAddress);
+        EditText name = findViewById(R.id.editTextName);
+        Spinner dropdown = findViewById(R.id.spinner);
 
         Bundle bundle = getIntent().getExtras();
         fridgeID = bundle.getString("fridgeID");
+        sEmail = bundle.getString("Email");
+        sName = bundle.getString("Name");
+        type = bundle.getString("Type");
 
-        Spinner dropdown = findViewById(R.id.spinner);
+        email.setText(sEmail);
+        name.setText(sName);
 
-        String[] items = new String[]{"DeliveryDriver", "Chef", "HeadChef"};
+
+        if (type.equals("Chef")) items = new String[]{"Chef", "DeliveryDriver", "HeadChef"};
+        else if (type.equals("HeadChef")) items = new String[]{"HeadChef", "DeliveryDriver", "Chef"};
+        else if (type.equals("DeliveryDriver")) items = new String[]{"DeliveryDriver", "Chef", "HeadChef"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 
@@ -59,25 +74,39 @@ public class addNewUserActivity extends AppCompatActivity implements AdapterView
     }
 
     public void onSubmitClicked(View view) {
-        EditText email = findViewById(R.id.editTextTextEmailAddress2);
         EditText name = findViewById(R.id.editTextName);
-
         sName = name.getText().toString();
-        sEmail = email.getText().toString();
         DocumentReference docRef = db.collection("Fridges/" + fridgeID + "/Users").document(sEmail);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
-                    if (document.exists()) Toast.makeText(addNewUserActivity.this, "User with this email address already exists!", Toast.LENGTH_SHORT).show();
-                    else if (!sEmail.contains("@gmail.com")) Toast.makeText(addNewUserActivity.this, "Wrong email type!", Toast.LENGTH_SHORT).show();
+                    if (!document.exists()) Toast.makeText(editUserActivity.this, "User with this email does not exists!", Toast.LENGTH_SHORT).show();
+                    else if (!sEmail.contains("@gmail.com")) Toast.makeText(editUserActivity.this, "Wrong email type!", Toast.LENGTH_SHORT).show();
                     else {
                         Map<String, Object> user = new HashMap<>();
                         user.put("Name", sName);
                         user.put("Type", type);
                         docRef.set(user);
-                        Toast.makeText(addNewUserActivity.this, "Added a new "+type+"!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(editUserActivity.this, "Succesfully changed details!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+    public void onDeleteClicked(View view) {
+        DocumentReference docRef = db.collection("Fridges/" + fridgeID + "/Users").document(sEmail);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (!document.exists()) Toast.makeText(editUserActivity.this, "User with this email does not exists!", Toast.LENGTH_SHORT).show();
+                    else {
+                        docRef.delete();
+                        Toast.makeText(editUserActivity.this, "Removed a "+type+"!", Toast.LENGTH_SHORT).show();
+                        finish();
                     }
                 }
             }
--- app/src/main/java/com/example/aadapplication/userAccessActivity.java
+++ app/src/main/java/com/example/aadapplication/userAccessActivity.java
@@ -16,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -31,51 +32,18 @@ public class userAccessActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         fridgeID = bundle.getString("fridgeID");
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("Fridges/"+fridgeID+"/Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                String[] emails = new String[task.getResult().size()];
-                String[] types = new String[task.getResult().size()];
-                String[] names = new String[task.getResult().size()];
-                if (task.isSuccessful()) {
-                    int i = 0;
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        emails[i] = document.getId();
-                        types[i]=document.getString("Type");
-                        names[i]=document.getString("Name");
-                        i++;
-                    }
-
-                } else {
-                    Log.d(TAG, "Error getting documents: ", task.getException());
-                }
-                ListView lView;
-
-                ListAdapterAccess lAdapter;
-
-                lView = (ListView) findViewById(R.id.androidList);
-
-                lAdapter = new ListAdapterAccess(userAccessActivity.this, names, emails, types);
-
-                lView.setAdapter(lAdapter);
-
-                lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        Toast.makeText(userAccessActivity.this, names[i]+" "+emails[i], Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
+        updateList();
     }
 
     @Override
     public void onRestart() {
         super.onRestart();
+        updateList();
+    }
+
+    public void updateList(){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("Fridges/"+fridgeID+"/Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        db.collection("Fridges/"+fridgeID+"/Users").orderBy("Type", Query.Direction.ASCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 String[] emails = new String[task.getResult().size()];
@@ -107,11 +75,23 @@ public class userAccessActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         Toast.makeText(userAccessActivity.this, names[i]+" "+emails[i], Toast.LENGTH_SHORT).show();
+
+                        Bundle bundle = getIntent().getExtras();
+                        fridgeID = bundle.getString("fridgeID");
+
+                        Intent myIntent = new Intent(userAccessActivity.this, editUserActivity.class);
+                        myIntent.putExtra("fridgeID", fridgeID);
+                        myIntent.putExtra("Name", names[i]);
+                        myIntent.putExtra("Email", emails[i]);
+                        myIntent.putExtra("Type", types[i]);
+
+                        startActivity(myIntent);
                     }
                 });
             }
         });
     }
+
     public void toAddNewUserActivity(View view){
         Bundle bundle = getIntent().getExtras();
         fridgeID = bundle.getString("fridgeID");

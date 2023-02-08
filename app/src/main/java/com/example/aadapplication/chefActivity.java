2ecf8230bb0fd613acd8cebadf8cbacd0cdbaf65
pawel <N0939175@my.ntu.ac.uk>
2023-02-08 04:08:41 +0000

    Added functionality to chefs and delivery drivers

--- app/src/main/java/com/example/aadapplication/chefActivity.java
+++ app/src/main/java/com/example/aadapplication/chefActivity.java
@@ -1,16 +1,51 @@
 package com.example.aadapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class chefActivity extends AppCompatActivity {
 
+    private String email;
+    private String fridgeID;
+    private String name;
+    private TextView fridgeTextID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Bundle bundle = getIntent().getExtras();
-        String email = bundle.getString("message");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef);
+
+        Bundle bundle = getIntent().getExtras();
+        email = bundle.getString("email");
+        name = bundle.getString("name");
+        fridgeID = bundle.getString("fridgeID");
+
+        fridgeTextID = findViewById(R.id.fridgeIDtext2);
+        fridgeTextID.setText("Fridge #"+fridgeID);
+    }
+
+    public void insertItemButton(View view) {
+        Intent myIntent = new Intent(chefActivity.this, barcode_scanner.class);
+        myIntent.putExtra("name", name);
+        myIntent.putExtra("fridgeID", fridgeID);
+        startActivity(myIntent);
+
+    }
+
+    public void checkStockButton(View view) {
+        Intent myIntent = new Intent(chefActivity.this, checkStockActivity.class);
+        myIntent.putExtra("fridgeID", fridgeID);
+        startActivity(myIntent);
+    }
+
+    public void removeItemButton(View view) {
+        Intent myIntent = new Intent(chefActivity.this, Remove_barcode_scanner.class);
+        myIntent.putExtra("fridgeID", fridgeID);
+        myIntent.putExtra("name", name);
+        startActivity(myIntent);
     }
 }
--- app/src/main/java/com/example/aadapplication/deliveryDriverActivity.java
+++ app/src/main/java/com/example/aadapplication/deliveryDriverActivity.java
@@ -1,16 +1,38 @@
 package com.example.aadapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class deliveryDriverActivity extends AppCompatActivity {
 
+    private String email;
+    private String fridgeID;
+    private String name;
+    private TextView fridgeTextID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Bundle bundle = getIntent().getExtras();
-        String email = bundle.getString("message");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_driver);
+
+        Bundle bundle = getIntent().getExtras();
+        email = bundle.getString("email");
+        name = bundle.getString("name");
+        fridgeID = bundle.getString("fridgeID");
+
+        fridgeTextID = findViewById(R.id.fridgeIDtext3);
+        fridgeTextID.setText("Fridge #"+fridgeID);
+    }
+
+    public void insertItemButton(View view) {
+        Intent myIntent = new Intent(deliveryDriverActivity.this, barcode_scanner.class);
+        myIntent.putExtra("name", name);
+        myIntent.putExtra("fridgeID", fridgeID);
+        startActivity(myIntent);
+
     }
 }
